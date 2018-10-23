7bba9acb27cde59589d2a759643ff447b688076d
Unknown <unknown@example.com>
2018-10-23 13:46:36 +0100

    classes

---
+++ Epione_JEE-ejb/src/main/java/tn/esprit/pi/epione/persistence/Admin.java
@@ -0,0 +1,10 @@
+package tn.esprit.pi.epione.persistence;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Admin extends User {
+	
+	
+
+}
---
+++ Epione_JEE-ejb/src/main/java/tn/esprit/pi/epione/persistence/Sexe.java
@@ -0,0 +1,7 @@
+package tn.esprit.pi.epione.persistence;
+
+public enum Sexe {
+	
+	male, female, other
+
+}
