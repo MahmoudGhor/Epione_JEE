d2e5767d6ba1ed1d7055c235953a01b45ca71833
Mahmoud <mahmoud.ghorbel@esprit.tn>
2018-10-23 23:29:03 +0100

    ajout attribut username

--- Epione_JEE-ejb/src/main/java/tn/esprit/pi/epione/persistence/User.java
+++ Epione_JEE-ejb/src/main/java/tn/esprit/pi/epione/persistence/User.java
@@ -18,6 +18,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	private String username;
 	private String firstname;
 	private String lastname;
 	@Temporal(TemporalType.TIMESTAMP)
@@ -111,5 +112,12 @@ public class User {
 	public void setActive(boolean active) {
 		this.active = active;
 	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 
 }
