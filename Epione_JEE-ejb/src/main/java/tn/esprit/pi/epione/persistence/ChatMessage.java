f350566ba22ccf8b4e0ae14962d9e956fb3fb55f
Med ben Malek <mohamed.benmalekcherif@esprit.tn>
2018-10-28 17:31:27 +0100

    chat patient - doctors (Questions)

---
+++ Epione_JEE-ejb/src/main/java/tn/esprit/pi/epione/persistence/ChatMessage.java
@@ -0,0 +1,32 @@
+package tn.esprit.pi.epione.persistence;
+
+import java.util.Date;
+
+public class ChatMessage {
+	
+	private String content;
+    private String sender;
+    private Date received;
+    
+    
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public Date getReceived() {
+		return received;
+	}
+	public void setReceived(Date received) {
+		this.received = received;
+	}
+    
+
+}
